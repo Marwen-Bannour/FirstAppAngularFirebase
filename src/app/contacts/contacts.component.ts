227815597aa0e6d4513784341b8f77ce55165317
Marwen Bannour <bannour.marwen.92@gmail.com>
2020-03-11 23:23:25 +0100

    fixed open/close addContact component

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
-
+  cols = 2 ;
   constructor() { }
 
   ngOnInit() {
   }
-
+  openAdd(){
+    this.cols = 3 ;
+  }
+  closeAdd(){
+    this.cols = 2 ;
+  }
 }
