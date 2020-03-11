40f58fdc8defe9e946d177516d6f847c18404d86
Marwen Bannour <bannour.marwen.92@gmail.com>
2020-03-11 23:30:11 +0100

    animated open/close addContact component

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import{trigger, transition, animate, style, state} from '@angular/animations';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
+  animations:[
+    trigger('flyInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateX(+100%)' }),
+        animate(500)
+      ]),
+      transition('* => void', [
+        animate(100, style({ transform: 'translateX(100%)' }))
+      ])
+    ])
+  ]
 })
 export class ContactsComponent implements OnInit {
   cols = 2 ;
