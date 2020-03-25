21c3dc57a8676fd2278eee07fe5ee4fc5ea11afb
Marwen-bannour <bannour.marwen.92@gmail.com>
2020-03-25 21:34:14 +0100

    update angular material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,28 +1,24 @@
 import { NgModule } from '@angular/core';
-import 
-  {
-    MatButtonModule,
-    MatButtonToggleModule ,
-    MatIconModule,
-    MatBadgeModule,
-    MatProgressSpinnerModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatMenuModule,
-    MatListModule,
-    MatDividerModule,
-    MatGridListModule,
-    MatExpansionModule,
-    MatCardModule,
-    MatTabsModule,
-    MatStepperModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatTableModule,
-    MatTooltipModule,
-    MatSortModule
-
-  } from '@angular/material';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 
