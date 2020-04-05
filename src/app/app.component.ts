61909a455dea786321d76942d078a0a7d520f890
Marwen-Bannour <bannour.marwen.92@gmail.com>
2020-04-05 19:11:30 +0200

    animate network test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { ConnectionService} from 'ng-connection-service'
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] ,
+  animations:[
+    trigger('anmCheck', [
+      state('in', style({ transform: 'translateY(0)'})),
+      transition('void => *', [
+        style({ transform: 'translateY(100%)'}),
+        animate(500)
+      ]),
+      transition('* => void', [
+        animate(500, style({ transform: 'translateY(100%)' }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'ExpMaterialWithFireb';
